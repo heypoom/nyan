4a44269d5b50a1b46bdad4622adf94a1fc4062b7
Pmc Dev <phoomparin@gmail.com>
2018-07-04 14:05:53 +0700

    :sparkles: Added lolcat mode

--- main.go
+++ main.go
@@ -13,7 +13,15 @@ func rgb(r, g, b int) string {
 	return fmt.Sprintf("\x1b[38;2;%d;%d;%dm", r, g, b)
 }
 
-func HSLToRGB(h, s, l float64) (r, g, b int) {
+func lolcatify(i int) (int, int, int) {
+	var f = 0.1
+
+	return int(math.Sin(f*float64(i)+0)*127 + 128),
+		int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
+		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+}
+
+func hslToRGB(h, s, l float64) (r, g, b int) {
 	var fR, fG, fB float64
 
 	if s == 0 {
@@ -57,7 +65,7 @@ func hueToRGB(p, q, t float64) float64 {
 	return p
 }
 
-func rainbow(input string, repeated bool) {
+func rainbow(input string, repeated bool, lolcat bool) {
 	steps := strings.Split(input, "")
 
 	for i, step := range steps {
@@ -70,7 +78,14 @@ func rainbow(input string, repeated bool) {
 			h = float64(i) / float64(len(steps))
 		}
 
-		r, g, b := HSLToRGB(h, 1.0, 0.8)
+		var r, g, b int
+
+		if lolcat {
+			r, g, b = lolcatify(i)
+		} else {
+			r, g, b = hslToRGB(h, 1.0, 0.8)
+		}
+
 		color := rgb(r, g, b)
 
 		fmt.Printf("%s%s", color, step)
@@ -84,12 +99,15 @@ func check(e error) {
 }
 
 var isRepeated bool
+var isLolcat bool
 
 func input() string {
 	var data []byte
 	var err error
 
-	flag.BoolVar(&isRepeated, "repeated", false, "Make colors look very repeated, kinda like temple events")
+	flag.BoolVar(&isRepeated, "repeated", false, "Make colors look very repeated, kinda like the lights in temple fairs")
+
+	flag.BoolVar(&isLolcat, "lolcat", false, "Make colors look rainbow-ish like lolcats")
 
 	flag.Parse()
 
@@ -112,5 +130,5 @@ func input() string {
 func main() {
 	words := input()
 
-	rainbow(words, isRepeated)
+	rainbow(words, isRepeated, isLolcat)
 }
